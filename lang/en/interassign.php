65b617f53514cbbc5cea08196bcf2ec258783ea6
Manuel Alejandro Moscoso Dominguez <manuel.moscoso.d@gmail.com>
2016-12-22 11:53:34 -0300

    first commit

---
+++ lang/en/interassign.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for interassign
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_interassign
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'interassign';
+$string['modulenameplural'] = 'interassigns';
+$string['modulename_help'] = 'Use the interassign module for... | The interassign module allows...';
+$string['interassign:addinstance'] = 'Add a new interassign';
+$string['interassign:submit'] = 'Submit interassign';
+$string['interassign:view'] = 'View interassign';
+$string['interassignfieldset'] = 'Custom example fieldset';
+$string['interassignname'] = 'interassign name';
+$string['interassignname_help'] = 'This is the content of the help tooltip associated with the interassignname field. Markdown syntax is supported.';
+$string['interassign'] = 'interassign';
+$string['pluginadministration'] = 'interassign administration';
+$string['pluginname'] = 'interassign';
